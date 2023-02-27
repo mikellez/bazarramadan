aa03c5928b91d2f12a16117da062b6f6e525d33c
Chin Zu Yao <zuyao@Chins-MacBook-Pro.local>
2023-02-27 10:01:24 +0800

    change register loc

---
+++ frontend/controllers/DaftarController.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace frontend\controllers;
+
+use frontend\models\ResendVerificationEmailForm;
+use frontend\models\VerifyEmailForm;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Daftar controller
+ */
+class DaftarController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout', 'faq'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['get'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        //$this->layout = 'main';
+        $model = new LoginForm();
+        if(Yii::$app->request->post()) {
+
+            if ($model->load(Yii::$app->request->post()) && $model->login()) {
+                Yii::$app->session->setFlash('success', '
+                    <div class="mt-3 alert alert-success" role="alert">
+                        <p><b>Tahniah!</b> MyKad / Kad Pengenalan anda: '.Yii::$app->user->identity->ic_no.' telah berdaftar dengan PBT. Anda layak memohon.</p>
+                    </div>
+                    ');
+                //return $this->goBack();
+                return $this->redirect('/dashboard');
+            } else {
+                $model->ic_no = '';
+
+                Yii::$app->session->setFlash('error', '
+                    <div class="mt-3 alert alert-danger" role="alert">
+                        <p><b>Maaf!</b> Nombor MyKad/Kad Pengenalan tidak berdaftar dengan mana-mana PBT.</p>
+                    </div>
+                ');
+
+                return $this->render('index', [
+                    'model' => $model,
+                ]);
+            }
+
+        }
+
+        return $this->render('index', [
+            'model' => $model
+        ]);
+    }
+
+    /**
+     * Displays add lisiting.
+     *
+     * @return mixed
+     */
+    public function actionAddListing()
+    {
+        $this->layout = 'empty';
+        return $this->render('add-listing');
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logs out the current user.
+     *
+     * @return mixed
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return mixed
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
+                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+            } else {
+                Yii::$app->session->setFlash('error', 'There was an error sending your message.');
+            }
+
+            return $this->refresh();
+        }
+
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return mixed
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return mixed
+     */
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
+            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
+            return $this->goHome();
+        }
+
+        return $this->render('signup', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Requests password reset.
+     *
+     * @return mixed
+     */
+    public function actionRequestPasswordReset()
+    {
+        $model = new PasswordResetRequestForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+
+                return $this->goHome();
+            }
+
+            Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for the provided email address.');
+        }
+
+        return $this->render('requestPasswordResetToken', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Resets password.
+     *
+     * @param string $token
+     * @return mixed
+     * @throws BadRequestHttpException
+     */
+    public function actionResetPassword($token)
+    {
+        try {
+            $model = new ResetPasswordForm($token);
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
+            Yii::$app->session->setFlash('success', 'New password saved.');
+
+            return $this->goHome();
+        }
+
+        return $this->render('resetPassword', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Verify email address
+     *
+     * @param string $token
+     * @throws BadRequestHttpException
+     * @return yii\web\Response
+     */
+    public function actionVerifyEmail($token)
+    {
+        try {
+            $model = new VerifyEmailForm($token);
+        } catch (InvalidArgumentException $e) {
+            throw new BadRequestHttpException($e->getMessage());
+        }
+        if (($user = $model->verifyEmail()) && Yii::$app->user->login($user)) {
+            Yii::$app->session->setFlash('success', 'Your email has been confirmed!');
+            return $this->goHome();
+        }
+
+        Yii::$app->session->setFlash('error', 'Sorry, we are unable to verify your account with provided token.');
+        return $this->goHome();
+    }
+
+    /**
+     * Resend verification email
+     *
+     * @return mixed
+     */
+    public function actionResendVerificationEmail()
+    {
+        $model = new ResendVerificationEmailForm();
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
+                return $this->goHome();
+            }
+            Yii::$app->session->setFlash('error', 'Sorry, we are unable to resend verification email for the provided email address.');
+        }
+
+        return $this->render('resendVerificationEmail', [
+            'model' => $model
+        ]);
+    }
+
+    public function actionFaq() {
+        return $this->render('faq');
+    }
+}
---
+++ frontend/views/daftar/index.php
@@ -0,0 +1,110 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+$this->title = 'Bazar Ramadan Plats Selangor';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="site-heading display-4">
+            <img width="300" height="300" src="<?= Yii::$app->params['backendUrl']?>/storage/Logo PLATS Ramadan.png"/>
+        </h1>
+        <!--<h1 class="site-heading display-4">Bazar Ramadan</h1>-->
+
+        <div class="lead mb-3">
+            <img src="<?=Yii::$app->params['backendUrl'].'/storage/platselangor_logo@2x.png'?>" width="200px"/>
+        </div>
+
+        <?php $form = ActiveForm::begin([
+            'id' => 'login-form',
+            'fieldConfig' => [
+                'template' => "{input}"
+            ],
+        ]); ?>  
+
+
+        <p class="">SILA MASUKKAN NOMBOR KAD PENGENALAN ANDA</p>
+        <br/>
+
+        <?= $form->field($model, 'ic_no', [
+            'options'=>[
+                'class'=>'',
+            ],
+            'template'=>'{input}'
+            
+        ])->textInput([ 'autofocus' => true, 'placeholder'=>'No Kad Pengenalan']) ?>
+        <!--<hr class="mt-5" style="border-top: 2px solid #d39e00">-->
+        <br/>
+
+        <p align="left" style="font-style: italic;">Contoh: 800323105512</p>
+
+        <p align="left" class="text-sm" style="font-style: italic;">* Hanya peniaga yang mempunyai permit bazar Ramadan dari Pihak Berkuasa Tempatan layak membuka kedai di Bazar Ramandan PLATS.</p>
+
+        <div class="">
+        </div>
+
+
+        <div class="d-flex justify-content-center">
+            <!--<a class="btn btn-md btn-success mt-3" href="/">Semak Kelayakan</a>-->
+            <?= Html::submitButton('Semak Kelayakan', ['class' => 'btn btn-sm btn-success mt-3', 'style'=>'background: #f37a20; border-color: #f37a20;', 'name' => 'login-button']) ?>
+        </div>
+
+        <?= Yii::$app->session->getFlash('success')?>
+        <?= Yii::$app->session->getFlash('error')?>
+
+        <?php ActiveForm::end(); ?>
+
+        <div class="d-flex justify-content-center">
+            <a href="/site/faq" target="_blank" style="color:black"><u class="mt-3">Soalan Lazim</u></a>
+        </div>
+        <div class="d-flex justify-content-center">
+            <p class="mt-3 text-muted">PLATS adalah sebuah Inisiatif Kerajaan Selangor di bawah</p>
+        </div>
+        <div class="d-flex justify-content-center">
+            <img src="<?=Yii::$app->params['backendUrl'].'/storage/mbi_pnsb_logo@2x.png'?>" width="220px"/>
+        </div>
+        <div class="d-flex justify-content-center">
+            <p class="mt-3 text-muted">dengan kerjasama Pihak Berkuasa Tempatan Selangor</p>
+        </div>
+        <div class="d-flex justify-content-center d-lg-none">
+            <!--<div class="row">-->
+            <?php foreach(\common\models\PbtLocation::find()->all() as $location):?>
+                <!--<div class="col-sm-4">
+                    <img width="30" height="30" src="<?= Yii::$app->params['backendUrl']?>/storage/<?= $location->code?>.png"/>
+                </div>-->
+            <?php endforeach;?>
+            <!--</div>-->
+            <?php 
+                $items = \common\models\PbtLocation::find()->all();
+                $numberOfColumns = 3;
+                $bootstrapColWidth = 12 / $numberOfColumns ;
+            
+                $arrayChunks = array_chunk($items, $numberOfColumns);
+                foreach($arrayChunks as $items) {
+                    echo '<div class="row">';
+                    foreach($items as $item) {
+                        echo '<div class="col-lg-12 col-md-'.$bootstrapColWidth.'">';
+                        // your item
+                        echo '<img width="30" height="30" src="'.Yii::$app->params['backendUrl'].'/storage/'.strtolower($item->code).'.png"/>';
+                        echo '</div>';
+                    }
+                    echo '</div>';
+                }   
+            ?>
+            <!--<img src="<?=Yii::$app->params['backendUrl'].'/storage/pbt_logo@2x.png'?>" width="400px"/>-->
+        </div>
+        <div class="d-none d-lg-block text-center">
+            <?php foreach(\common\models\PbtLocation::find()->all() as $location):?>
+                    <img width="30" height="30" src="<?= Yii::$app->params['backendUrl']?>/storage/<?= strtolower($location->code)?>.png"/>
+                <!--<div class="col-sm-4">
+                    <img width="30" height="30" src="<?= Yii::$app->params['backendUrl']?>/storage/<?= $location->code?>.png"/>
+                </div>-->
+            <?php endforeach;?>
+        </div>
+    </div>
+
+</div>
